0e8bca793e54fe52a86f3c9fef96096c92aa368b
robertcolic <colicr@gmail.com>
2026-03-08 23:31:03 +0100

    fix user_loader

--- main.py
+++ main.py
@@ -23,10 +23,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-
 @login_manager.user_loader
 def load_user(user_id):
-    return db.get_or_404(User, user_id)
+    return db.session.get(User, int(user_id))
 
 
 # For adding profile images to the comment section
